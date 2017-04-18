c283a08df7ead6b63cf0e79458c9c1f13ee67ac4
Vera Nemedy-Varga <n.v.vera@gmail.com>
2017-04-18 11:18:33 +0200

    Card and Deck classses created, deck picks radnom cards

--- blackjack/black_jack.py
+++ blackjack/black_jack.py
@@ -10,13 +10,42 @@
 # We should be able to draw the top card which returns the drawn card and also removes it from the deck
 # Deck should be represented as string in this format:
 # 12 cards -  3 Clubs, 3 Diamonds, 3 Hearts, 3 Spades
-deck = Deck(12)
+
+import random
+
+class Card(object):
+    def __init__(self):
+        self.color = ["Hearts", "Diamonds", "Spades", "Clubs"]
+        self.value = range(1, 13)
+        self.random_value = random.randint(1, 13)
+        self.random_color = random.choice(self.color)
+        self.chosen_card = str(self.random_value) + " " + str(self.random_color)
+
+
+class Deck(object):
+    def __init__(self, number_of_cards):
+        self.number_of_cards = number_of_cards
+        self.list_of_cards = []
+        for n in range(self.number_of_cards):
+            self.list_of_cards.append(Card().chosen_card)
+
+
+
+
+card = Card()
+
+deck = Deck(4)
+print(card.chosen_card)
+print(deck.list_of_cards)
+
 print(deck)
 # Should print out:
 # 12 cards -  3 Clubs, 3 Diamonds, 3 Hearts, 3 Spades
-top_card = deck.draw()
-print(top_card)
-print(deck)
+
+# top_card = deck.draw()
+# print(top_card)
+# print(deck)
+
 # Should print out:
 # Queen Spades
 # 11 cards - 3 Clubs, 3 Diamonds, 3 Hearts, 2 Spades
