8576d75831280ec242fc8b5438fb5afe736401a7
Vera Nemedy-Varga <n.v.vera@gmail.com>
2017-04-18 12:08:59 +0200

    appends copied text to destination's text

--- copy/copy.py
+++ copy/copy.py
@@ -26,6 +26,7 @@ class Controller(object):
         if len(self.list_argv) == 0:
             View().print_usage()
         elif len(self.list_argv) >= 1 and (''.join(self.list_argv[1:])):
+            self.argument_list = (''.join(self.list_argv[1:]))
             Model().copy_file()
         elif len(self.list_argv) >= 1:
             View().print_with_one_argument_provided()
@@ -33,8 +34,15 @@ class Controller(object):
 class Model(object):
 
     def copy_file(self):
-        pass
-
+        self.destination = open("destination.txt", 'a+')
+        self.read_lines_from_destination = self.destination.readlines()
+        self.source = open("source.txt", 'r')
+        self.read_lines_from_file = self.source.readlines()
+        for lines in self.read_lines_from_file:
+            self.destination.write("\n" + lines)
+        self.source.close()
+        self.destination.close()
+        
 class View(object):
 
     def print_usage(self):
@@ -44,4 +52,5 @@ class View(object):
         print("No destination provided")
 
 controller = Controller()
+model = Model()
 view = View()
