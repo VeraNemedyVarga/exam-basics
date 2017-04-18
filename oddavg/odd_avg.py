85558c29f2b9776548db779557e886f2d85f7ff8
Vera Nemedy-Varga <n.v.vera@gmail.com>
2017-04-18 09:30:03 +0200

    odd_average function created

--- oddavg/odd_avg.py
+++ oddavg/odd_avg.py
@@ -1,3 +1,9 @@
 # Create a function called `odd_average` that takes a list of numbers as parameter
 # and returns the average value of the odd numbers in the list
 # Create basic unit tests for it with at least 3 different test cases
+
+numlist = [2, 3, 4, 5]
+def odd_average(numlist):
+    return sum(numlist) / len(numlist)
+
+print(odd_average(numlist))
