94b6e73870f1f986a571a349afd271882d2f7612
Vera Nemedy-Varga <n.v.vera@gmail.com>
2017-04-18 11:27:33 +0200

    prints usage info if no argument is provided

--- copy/copy.py
+++ copy/copy.py
@@ -6,3 +6,31 @@
 # No destination provided
 # When both arguments provided and the source is a file
 # Read all contents from it and write it to the destination
+
+import sys
+
+class Controller(object):
+
+    def __init__(self):
+        self.list_argv = []
+        self.arg_reader()
+        self.controller()
+
+    def arg_reader(self):
+        if len(sys.argv) <= 1:
+            self.list_argv = []
+        else:
+            self.list_argv = sys.argv[1:]
+
+    def controller(self):
+        if len(self.list_argv) == 0:
+            View().print_usage()
+
+
+class View(object):
+
+    def print_usage(self):
+        print("copy [source] [destination]")
+
+controller = Controller()
+view = View()
