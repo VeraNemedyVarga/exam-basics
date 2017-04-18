f8f1b476ae8efba75d4a3e4276d987c6722a1be4
Vera Nemedy-Varga <n.v.vera@gmail.com>
2017-04-18 10:20:54 +0200

    finally understood exercise correctly, function created

--- oddavg/odd_avg.py
+++ oddavg/odd_avg.py
@@ -2,8 +2,20 @@
 # and returns the average value of the odd numbers in the list
 # Create basic unit tests for it with at least 3 different test cases
 
-numlist = [2, 3, 4, 5]
-def odd_average(numlist):
-    return sum(numlist) / len(numlist)
 
-print(odd_average(numlist))
+class CountAverage(object):
+
+    def odd_average(self, numlist):
+        self.list_of_odd_numbers = []
+        self.numlist = numlist
+        if len(self.numlist) == 1 and self.numlist[0] % 2 == 0:
+            return "ZeroDivisionError, element is even in list"
+
+        elif len(self.numlist) != 0:
+            for self.number in self.numlist:
+                if self.number % 2 == 1:
+                    self.list_of_odd_numbers.append(self.number)
+            return sum(self.list_of_odd_numbers)/ len(self.list_of_odd_numbers)
+
+        else:
+            return "ZeroDivisionError, please do not enter empty list"
